f210e2e52e9dbbce08ddd37c094b5830e93e4b2a
성기범 <133200703+sungkibum@users.noreply.github.com>
2024-11-27 15:59:18 +0900

    TDD를 위한 assertj dependency 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    // https://mvnrepository.com/artifact/org.assertj/assertj-core
+    testImplementation("org.assertj:assertj-core:3.26.3")
+
 }
 
 tasks.test {
