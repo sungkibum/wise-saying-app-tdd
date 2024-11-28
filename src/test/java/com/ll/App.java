7da7c5fe8c6745faf4ca432adfb2bf3c64e36187
성기범 <133200703+sungkibum@users.noreply.github.com>
2024-11-28 11:38:34 +0900

    명언 앱 실행 시 "== 명언 앱 ==" 초기 메시지 출력 테스트 코드

---
+++ src/test/java/com/ll/App.java
@@ -0,0 +1,15 @@
+package com.ll;
+
+import java.util.Scanner;
+
+public class App {
+    private final Scanner sc;
+    public App(Scanner sc) {
+        this.sc = sc;
+    }
+
+    public void run() {
+
+        System.out.println("== 명언 앱 ==");
+    }
+}
