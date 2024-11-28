e06e2c26cc44fd8dee0ab02cf90fe94ca4cfffe0
성기범 <133200703+sungkibum@users.noreply.github.com>
2024-11-28 11:28:46 +0900

    TestUtil 코드 작성

--- src/test/java/com/ll/standard/util/TestUtil.java
+++ src/test/java/com/ll/standard/util/TestUtil.java
@@ -1,9 +1,28 @@
 package com.ll.standard.util;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class TestUtil {
     public static Scanner getScanner(String input) {
         return new Scanner(input);
+    }
+
+    public static ByteArrayOutputStream setOutToByteArray() {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        return output;
+    }
+
+    public static void clearSetOutToByteArray(ByteArrayOutputStream byteArrayOutputStream) {
+        System.setOut(new PrintStream(new ByteArrayOutputStream()));
+        try {
+            byteArrayOutputStream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/test/java/com/ll/standard/util/TestUtilTest.java
+++ src/test/java/com/ll/standard/util/TestUtilTest.java
@@ -3,13 +3,14 @@ package com.ll.standard.util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TestUtilTest {
     @Test
-    @DisplayName("TestUtil.getScanner()")
+    @DisplayName("TestUtil.genScanner()")
     public void t1() {
         Scanner scanner = TestUtil.getScanner("""
                 등록
@@ -24,5 +25,19 @@ public class TestUtilTest {
         assertThat(cmd).isEqualTo("등록");
         assertThat(content).isEqualTo("나의 죽음을 적들에게 알리지 말라!");
         assertThat(author).isEqualTo("이순신");
+    }
+
+    @Test
+    @DisplayName("TestUtil.setOutToByteArray()")
+    public void t2() {
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
+
+        System.out.println("2 / 이순신 / 나의 죽음을 적들에게 알리지 말라!");
+
+        String out = byteArrayOutputStream.toString().trim();
+        TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
+
+        assertThat(out).isEqualTo("2 / 이순신 / 나의 죽음을 적들에게 알리지 말라!");
+        System.out.println("이제는 화면에 출력됩니다.");
     }
 }
