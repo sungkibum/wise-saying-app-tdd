16464a448c02ad345bf18c7f1a2b06d343e9f871
성기범 <133200703+sungkibum@users.noreply.github.com>
2024-12-02 16:15:45 +0900

    모니터 출력, 미출력 코드

--- src/main/java/org/example/ll/Main.java
+++ src/main/java/org/example/ll/Main.java
@@ -1,14 +1,16 @@
 package org.example.ll;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        lab1();
-        lab2();
+//        lab1();
+//        lab2();
+//        lab3();
+        lab4();
     }
+
 
 
 
@@ -29,4 +31,31 @@ public class Main {
 
         System.out.println("입력한 명령: " + cmd);
     }
+    private static void lab3() {
+        System.out.println("안녕하세요.");
+    }
+    private static void lab4() {
+        System.out.println("== 시작 ==");
+
+        //출력이 모니터로 안되도록 설정
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        System.out.println("안녕하세요.");   //안녕하세요는 output안에 쌓임
+
+        //다시 모니터로 출력되도록 설정
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+
+        System.out.println("== 끝 ==");
+
+        System.out.println("출력 : " + output.toString());
+
+        //output 자원 해제
+        try {
+            output.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
