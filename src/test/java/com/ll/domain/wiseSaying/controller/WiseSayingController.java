c862bacd2d3d5c5fa1a0208453631423b111c97a
성기범 <133200703+sungkibum@users.noreply.github.com>
2024-12-02 17:50:12 +0900

    등록될 때마다 명언 번호 증가 테스트 코드 작성

--- src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingController.java
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingController.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 
 public class WiseSayingController {
     private final Scanner sc;
+    private int lastId;
     public WiseSayingController(Scanner sc) {
         this.sc = sc;
+        this.lastId = 0;
     }
 
     public void actionAdd() {
         System.out.println("명언 : ");
         System.out.println("작가 : ");
-        System.out.println("1번 명언이 등록되었습니다.");
+
+        int id = ++lastId;
+        System.out.println(id + "번 명언이 등록되었습니다.");
     }
 }
