2d67cf5ad7d1e250dfdb6683cc5214ae1fc5168b
성기범 <133200703+sungkibum@users.noreply.github.com>
2024-12-02 16:02:53 +0900

    선입력 메소드

--- src/main/java/org/example/ll/Main.java
+++ src/main/java/org/example/ll/Main.java
@@ -1,5 +1,7 @@
 package org.example.ll;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class Main {
@@ -8,9 +10,7 @@ public class Main {
         lab2();
     }
 
-    private static void lab2() {
 
-    }
 
     private static void lab1() {
         Scanner scanner = new Scanner(System.in);
@@ -19,6 +19,14 @@ public class Main {
         String cmd = scanner.nextLine().trim();
 
         System.out.println("입력한 명령: " + cmd);
+    }
 
+    private static void lab2() {
+        InputStream inputStream = new ByteArrayInputStream("명령".getBytes());    //선입력 '명령' 받아옴
+        Scanner scanner = new Scanner(inputStream); //선입력 '명령' 바로 대입
+        System.out.print("명령) ");
+        String cmd = scanner.nextLine().trim();
+
+        System.out.println("입력한 명령: " + cmd);
     }
 }
